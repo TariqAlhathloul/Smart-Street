2e44824dcf3a701463fadc11928b8709e63dca52
TariqAlhathloul <tariq@DESKTOP-UEDRNSP>
2024-09-16 17:39:02 +0300

    database integration

---
+++ Data/push_to_database.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from pymongo import MongoClient
+import os
+#dataset path
+csv_file_path = '../Data/Violations_detected.csv'
+
+#read the detected violations file
+df = pd.read_csv(csv_file_path)
+
+# connect to the database
+client = MongoClient(os.environ.get('stringcon'))
+
+# create a database and a collection
+db = client['DeepLearningCluster']  
+collection = db['Violations_detected']  
+
+#convert dataframe to dictionary
+data = df.to_dict(orient='records')
+
+#insert into the collection
+collection.insert_many(data)
+
+print("data inserted")
