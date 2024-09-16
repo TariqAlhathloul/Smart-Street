fce6306d15b768029aa4f4def2c74517b99dab22
TariqAlhathloul <tariq@DESKTOP-UEDRNSP>
2024-09-16 17:30:02 +0300

    update location

--- WorkSpace/get_location.py
+++ WorkSpace/get_location.py
@@ -1,39 +1,38 @@
 import geocoder
 import requests
-import os
 import json
 from geopy.geocoders import Nominatim
 
 """
 make a http request to get the current location of the raspberry pi
-then convert the response to json and extract the latitude and longitude
+then extract the latitude, longitude, city
 """
 
 def get_current_location():
     """
     No parameters.
-    the function will return the [latitude, longitude] of the camera.
+    the function will return the [latitude, longitude, city] of the camera.
     """
-    response = requests.get(os.getenv('GEO_KEY'))
-    if response.status_code != 200:
-        print(f"Error getting location, response code : {response.status_code}")
-    data = json.loads(response.content)
-    latitude = data['latitude']
-    longitude = data['longitude']
-    return latitude, longitude
-# الإمام عبدالله بن ثنيان آل سعود, الضباط, الزهراء, بلدية الملز, محافظة الرياض, منطقة الرياض, 12811, السعودية
+    response = requests.get('http://ipinfo.io/json')
+    data = response.json()
+    if 'loc' in data:
+        location = data['loc'].split(',')
+        latitude = location[0]
+        longitude = location[1]
+        return latitude, longitude
+    else:
+        return None, None
 def get_road_name():
     """
     No parameters.
     the function will return the road name of the camera.
     """
-    latitude, longitude = get_current_location()
+    latitude, longitude, _ = get_current_location()
     geolocator = Nominatim(user_agent="smart-street")
     location = geolocator.reverse(f"{latitude}, {longitude}")
-    zip_code = location.address.split(",")[6]
-    street_name = location.address.split(",")[0]
-    return street_name, zip_code
-# print(get_current_location())
+    road_name = location.address.split(",")[0]
+    return road_name
 
-# road_name = (get_road_name())
+# # print(get_current_location())
+# road_name = get_road_name()
 # print(road_name)
