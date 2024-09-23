450e8ab36fdd62a7faf2c21aff2f6e3276973a55
TariqAlhathloul <tariq@DESKTOP-UEDRNSP>
2024-09-23 11:09:17 +0300

    update

--- deployment/detect.py
+++ deployment/detect.py
@@ -1,7 +1,7 @@
 """
-the script detects vehicles that are overtaking in non-permitted areas and saves the violation information to a csv file
+the script detects vehicles that are overtaking in non-permitted areas,
+and saves the violation information to a csv file.
 """
-import numpy as np
 import cv2
 from ultralytics import YOLO
 import datetime as dt
@@ -46,10 +46,17 @@ def is_overtaking(vehicle_center, line_center):
     """
     the function checks if a vehicle is overtaking in non-permitted areas
     """
-    distance = vehicle_center[0] - line_center[0]
-    # distance threshold
-    is_overtaking = distance > 120 and vehicle_center[0] > line_center[0] and distance < 800
-    return (is_overtaking, distance)
+    if line_center[0] > 1250:
+        #meaning that the line is on the right side
+        distance = vehicle_center[0] - line_center[0]
+        # distance threshold
+        is_overtaking = distance > 120 and vehicle_center[0] > line_center[0] and distance < 800
+    else:
+        #meaning that the line is on the left side
+        distance = line_center[0] - vehicle_center[0]
+        # distance threshold
+        is_overtaking = vehicle_center[0] < line_center[0]
+    return is_overtaking
 
 #load the model
 model = YOLO('../Models/best.onnx', task='segment')
@@ -62,9 +69,10 @@ assert cap.isOpened(), 'Cannot capture video'
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(cap.get(cv2.CAP_PROP_FPS))
+print(f"width: {width}, height: {height}, fps: {fps}")
 
 #video writer
-output_path = '../resources/output_video(14).mp4'
+output_path = '../resources/outPut(2).mp4'
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
@@ -74,6 +82,7 @@ street_name = get_road_name()
 violation_type = 'overtaking'
 date = dt.datetime.now().strftime('%Y-%m-%d')
 license_plate_number = None
+
 #counter to index saved images
 counter = 0
 #skip frames for faster processing
@@ -83,12 +92,17 @@ skip_frames = 5
 line_center = (1, 1)
 vehicle_center = (1, 1)
 
+#{0: 'bus', 1: 'car', 2: 'license_plate', 3: 'solid-yellow-line', 4: 'truck'}
+vehicles = [0, 1, 2, 4]
+
+#start capturing frames
 while cap.isOpened():
-    #start reading framess
+
     success, frame = cap.read()
     if not success:
         break
 
+    #skip frames for faster processing
     #current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
     #if current_frame % skip_frames != 0:
         #continue
@@ -99,35 +113,36 @@ while cap.isOpened():
     current_time = dt.datetime.now()
     cv2.putText(frame, current_time.strftime('%Y-%m-%d %H:%M:%S'), (20, 1850), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 5)
 
-        #detect the solid-yellow line and thier center
+
+    #loop over the detected masks and boxes
     for mask, box in zip(results[0].masks.xy, results[0].boxes):
-        #  solid yellow lines detected
-        if box.cls[0].item() == 3: 
+        #solid-yellow-line detected
+        if 3 in box.cls: 
             line_center = calculate_center(box.xyxy)
+            #cv2.circle(frame, line_center, 15, (0, 0, 255), -1)
 
         # vehicle detected
-        else:
+        elif box.cls in vehicles:
             vehicle_center = calculate_center(box.xyxy)
-            overtaking, distance = is_overtaking(vehicle_center, line_center)
-            #check if the vehicle is overtaking and solid-yellow-line
+            overtaking = is_overtaking(vehicle_center, line_center)
+
+            #vehicle is overtaking and solid-yellow-line is detected
             if overtaking and line_center != (1, 1): 
                 cv2.putText(frame, "Violation Detected !", (20, 650), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-                cv2.putText(frame, f"line center {line_center}", (20, 700), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-                cv2.putText(frame, f"car center{vehicle_center}", (20, 750), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
-                print(f"distance {distance}")
+                #draw a circle on the vehicle center
+                #cv2.circle(frame, vehicle_center, 15, (255, 0, 0), -1)
                 #draw the bounding box on violated vehicle
                 frame = draw_bbox(frame, box, color=(0, 0, 255), thickness=10)
                 #crop detected violation image and save it
                 x1, y1, x2, y2 = box.xyxy[0]
                 cv2.imwrite(f'../resources/violations_images/violation{counter}.jpg', frame[int(y1):int(y2), int(x1):int(x2)])
                 counter += 1
-                #check if there the license plate is detected
-                if box.cls[0].item() == 2:
-                    #read license plate number
+
+                #license plate is detected
+                if 2 in box.cls:
                     #send croped image to ocr function
                     license_plate_number = read_license_plate(frame[int(y1):int(y2), int(x1):int(x2)], counter)
                     #get vehicle type
-                    #{0: 'bus', 1: 'car', 2: 'license_plate', 3: 'solid-yellow-line', 4: 'truck'}
                     vehicle_type = ['bus', 'license_plate','car', 'solid-yellow-line', 'truck'][int(box.cls[0].item())]
                     # append the violation data to the dataframe
                     with open('../Database/violations_detected.csv', 'a') as file:
@@ -136,4 +151,3 @@ while cap.isOpened():
 
 cap.release()
 out.release()
-# TODO: apply OCR on the saved image to get the license plate number and append it to the violations.csv file
