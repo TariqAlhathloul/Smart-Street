afd13ee1c62783d09e8fbeda3cb9b91b809ce66c
TariqAlhathloul <tareq17al.hathloul@gmail.com>
2024-10-02 08:36:16 +0100

    inster data

--- WorkSpace/insert_data.py
+++ WorkSpace/insert_data.py
@@ -5,7 +5,7 @@ def insert_data(date, time, license_plate_number, vehicle_type, violation_type, 
     try:
         with open('../Database/violations_detected.csv', 'a') as file:
             file.write(f"{date},{time},{license_plate_number},{vehicle_type},{violation_type},{latitude},{longitude},{street_name}\n")
-            print('Data inserted successfully.')
+            print('Data inserted')
 
     except Exception as e:
         print(f"Error: {e}")
