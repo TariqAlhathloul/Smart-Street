f1faa5e3c878fe668cb164c46005e1de1b305af7
TariqAlhathloul <tareq17al.hathloul@gmail.com>
2024-10-06 18:04:10 +0100

    add street name column

--- WorkSpace/insert_data.py
+++ WorkSpace/insert_data.py
@@ -1,6 +1,12 @@
 import os
 os.makedirs('../Database', exist_ok=True)
 
+
+"""
+the function inserts the violation data into a csv file 
+after data cleaning we will push it to the database
+"""
+
 def insert_data(date, time, license_plate_number, vehicle_type, violation_type, latitude, longitude, street_name):
     try:
         with open('../Database/violations_detected.csv', 'a') as file:
